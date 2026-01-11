8bcbc41d4c896d7a64c3498405d97f24fb33a219
MoneyMantra1 <minemantramc@gmail.com>
2026-01-10 19:07:04 -0500

    Add files via upload

---
+++ .github/workflows/src/main/java/dev/felnull/imp/explatform/forge/IMPPatchouliExpectPlatformImpl.java
@@ -0,0 +1,16 @@
+package dev.felnull.imp.explatform.forge;
+
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+
+public class IMPPatchouliExpectPlatformImpl {
+    public static void openBookGUI(ServerPlayer player, ResourceLocation location) {
+        //PatchouliAPI.get().openBookGUI(player, location);
+    }
+
+    public static ResourceLocation getOpenBookGui() {
+        // return PatchouliAPI.get().getOpenBookGui();e
+        return null;
+    }
+}
